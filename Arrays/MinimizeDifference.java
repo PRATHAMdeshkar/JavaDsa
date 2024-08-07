c5b08a0233388eb4c3c61a35a8c56786c1771dac
PRATHAMdeshkar <pratham.deshkar@gmail.com>
2024-08-07 17:55:57 +0530

    Minimize the maximum difference between the heights

---
+++ Arrays/MinimizeDifference.java
@@ -0,0 +1,16 @@
+//Minimize the maximum difference between the heights
+import java.util.*;
+public class MinimizeDifference {
+    
+    public static int getmindiff(int arr[], int k, int n) {
+        Arrays.sort(arr);
+
+        //****************************************
+        //****************************************
+        //************pending question************
+        //****************************************
+        //****************************************
+        
+        return arr[0];
+    }
+}
